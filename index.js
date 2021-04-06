fa5899552a78891669e4e6fe197632869430e3e5
Charles Grant Nichols <grant@inkdots.com>
2021-04-06 14:56:50 -0500

    commit tree 53aa80636bd23dfabd3eb593fc801fbcce01113d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+function saturdayFun(thing = 'roller-skate') {
+    return(`This Saturday, I want to ${thing}!`);
+}
+
+function mondayWork(thing = 'go to the office') {
+    return(`This Monday, I will ${thing}.`)
+}
+
+let wrapAdjective = function(style = "*") {
+    return function(adjective = "special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
